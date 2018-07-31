06c59a0e05cf7d976821e0c6720a7cfb3995f5ae
siem <siemarell@gmail.com>
2018-07-31 13:40:36 +0300

    task inheritance

--- tasks.py
+++ tasks.py
@@ -1,12 +1,11 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy import Enum as AEnum
 from enum import Enum, auto
 from abc import abstractmethod, ABCMeta
 import pywaves as pw
 
 Base = declarative_base()
-
 
 pw.setChain('testnet')
 
@@ -18,25 +17,34 @@ class TaskStatus(Enum):
     REWARDED = auto()
 
 
-class Task(Base, metaclass=ABCMeta):
-    __tablename__ = 'tasks'
+class Task(Base):  # , metaclass=ABCMeta):
+    __tablename__ = 'task'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False)
     status = Column(AEnum(TaskStatus))
+    type = Column(String(20))
 
-    # def __init__(self, owner):
-    #     self.owner = owner
-    #     self.status = TaskStatus.ADDED
+    __mapper_args__ = {
+        'polymorphic_identity': 'task',
+        'polymorphic_on': type
+    }
+
+    def __init__(self):
+        self.status = TaskStatus.ADDED
 
     @abstractmethod
-    def verify(self, data)-> bool:
+    def verify(self, data) -> bool:
         """Should verify the data and mark task completed if it passes"""
 
 
-class GetUserAddress(Task):
-    def __init__(self):
-        super().__init__()
-        self.address = None
+class GetUserAddressTask(Task):
+    __tablename__ = 'task_get_address'
+    id = Column(Integer, ForeignKey('task.id'), primary_key=True)
+    address = Column(String, nullable=True)
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'task_get_user_address',
+    }
 
     def verify(self, address):
         if pw.validateAddress(address):
@@ -45,4 +53,3 @@ class GetUserAddress(Task):
             return True
         else:
             return False
-
--- test/test_tasks.py
+++ test/test_tasks.py
@@ -1,22 +1,36 @@
 import unittest
 import db
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from tasks import Base, GetUserAddressTask, Task
 
 
 class TestTasks(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        db.init()
+    # engine = create_engine('sqlite:///:memory:')
+    # Session = sessionmaker(bind=engine)
 
-    def test_select(self):
-        self.assertEqual(db.get_last_update_index(), 0)
+    def setUp(self):
+        self.engine = create_engine('sqlite:///storage.db')
+        self.Session = sessionmaker(bind=self.engine)
+        self.session = self.Session()
+        Base.metadata.create_all(self.engine)
 
-    def test_update(self):
-        db.update_last_update_index(10)
-        self.assertEqual(db.get_last_update_index(), 10)
+    def test_create_get_address_task(self):
+        task = GetUserAddressTask()
+        self.session.add(task)
+        self.session.commit()
+        tasks = [task for task in self.session.query(Task)]
+        pass
 
-    @classmethod
-    def tearDownClass(cls):
-        db.update_last_update_index(0)
+        #self.assertEqual(db.get_last_update_index(), 0)
+    #
+    # def test_update(self):
+    #     db.update_last_update_index(10)
+    #     self.assertEqual(db.get_last_update_index(), 10)
+    #
+    # @classmethod
+    # def tearDownClass(cls):
+    #     db.update_last_update_index(0)
 
 if __name__ == '__main__':
     unittest.main()
